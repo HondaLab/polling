399489c248047f06da4bd8dbc2e77684645dbe53
Yasushi Honda <honda@csse.muroran-it.ac.jp>
2019-12-10 23:22:06 +0900

    Add files via upload

---
+++ send_example.py
@@ -0,0 +1,18 @@
+import socket
+import random
+import time
+import socket1a as sk
+
+if __name__=='__main__':
+
+   udp = sk.UDP_Send(sk.ROBOT_ADDR, sk.MPU9150_PORT)
+   data=[1] # 送信データ用のリスト
+   i=0 
+   start=time.time()
+   print("# 10秒間乱数をsendするテスト")
+   while time.time()-start<10 :
+      data[0] = random.randint(1,100000000)/10000000
+      print("%5.1f %s" % (time.time()-start,data[0]))
+      udp.send(data)
+      time.sleep(0.10)
+      i=i+1
