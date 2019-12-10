8dc86c2ea2bfa6c19d11e5a5eb5bc1a2650d6e6e
Yasushi Honda <honda@csse.muroran-it.ac.jp>
2019-12-10 23:01:06 +0900

    Create RealtimePlot.py

---
+++ RealtimePlot.py
@@ -0,0 +1,39 @@
+#
+import sock4robot3a as s4r
+from matplotlib import pyplot as plt
+import time
+import numpy as np
+
+print(s4r.BASE_ADDR, s4r.GYRO_PORT)
+udp=s4r.UDP_Recv(s4r.BASE_ADDR, s4r.GYRO_PORT)
+
+x=[0.0 for i in range(100)]
+y=[0.0 for i in range(100)]
+y1=[0.0 for i in range(100)]
+now=time.time()
+start=now
+plt.ylim(-2,2)
+plt.grid()
+
+
+lin, =  plt.plot(x,y,color='#ff0000')
+lin1, =  plt.plot(x,y1,color='#0000ff')
+while 1:
+   try:
+      data=udp.recv()
+      print("\r %7.2f %7.2f %4d" % (data[0],data[1],data[2]), end='')
+      x.pop(0)
+      x.append(now-start)
+      y.pop(0)
+      y.append(data[0])
+      y1.pop(0)
+      y1.append(data[1])
+      plt.xlim(min(x),max(x))
+      lin.set_xdata(x)
+      lin.set_ydata(y)
+      lin1.set_xdata(x)
+      lin1.set_ydata(y1)
+      plt.pause(0.0001)
+   except BlockingIOError:
+      pass
+   now=time.time()
